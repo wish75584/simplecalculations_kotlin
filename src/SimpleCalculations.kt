b3da70fcd9946866bce25844b4ec6264537ae56f
wish75584 <wish75584@gmail.com>
2020-10-10 09:05:04 +0530

    Initial commit

---
+++ src/SimpleCalculations.kt
@@ -0,0 +1,16 @@
+fun main(args : Array<String>){
+    val i = 10
+    val j = 20
+
+    var sum = i+j
+    var sub = i-j
+    var div = i/j
+    var mul = i*j
+
+    println("Sum is $sum")
+    println("Sunbtraction is : "+sub)
+    println("Multiplication is : "+mul)
+    println("Division is  : " +div)
+
+
+}
